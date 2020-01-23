1ff2917a2f5eca7d54fb6794b8cb212dddca7d06
vineet <vineet.gandham@gmail.com>
2020-01-23 20:37:06 +0530

    c3

--- lab3/q1.c
+++ lab3/q1.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+int opcount=0;
 void bubblesort(int *a,int n){
 	int temp;
 	for(int i=0;i<n-1;i++){
 		for(int j=0;j<n-i-1;j++){
+			opcount++;
 			if(a[j]>a[j+1]){
 				temp=a[j];
 				a[j]=a[j+1];
@@ -16,10 +18,14 @@ void main(){
 	int len,ele;
 	int *arr=malloc(sizeof(int)*len);
 	scanf("%d",&len);
+	printf("lenth is %d\n",len );
 	for(int i=0;i<len;i++){
 		scanf("%d",&ele);
 		arr[i]=ele;
 	}
+	//printf("?????\n");
 	bubblesort(arr,len);
+	printf("sorted array is: ");
 	for(int i=0;i<len;i++)printf("%d ",arr[i]);
+	printf("opcount is %d\n",opcount);
 }
--- lab3/q2.c
+++ lab3/q2.c
@@ -1,2 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+int opcount=0;
+int stringmatch(char *str,int len1,char *substr,int len2){
+	int flag=0;int k;
+	for(int i=0;i<=len1-len2;i++){
+		k=i;
+		for(int j=0;j<len2;j++){
+			opcount++;
+			if(str[k]!=substr[j])
+			break;
+			k++;
+			if(j==len2-1){
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1)break;
+	}
+	return flag;
+}
+void main(){
+	char* str=malloc(sizeof(char)*30);
+	char* substr=malloc(sizeof(char)*30);
+	scanf("%s",str);
+	scanf("%s",substr);
+	//printf("length of string : %ld\n",strlen(str) );
+	int flag=stringmatch(str,strlen(str),substr,strlen(substr));
+	if(flag)printf("substring found\n");else printf("substring not found\n");
+	printf("opcount is: %d\n",opcount);
+
+}
