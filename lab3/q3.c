ec503e2e368caa556c1b6464a661bfe5f45d17db
vineet <vineet.gandham@gmail.com>
2020-01-23 21:44:46 +0530

    c1

---
+++ lab3/q3.c
@@ -0,0 +1,38 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int partitionprob(int * arr,int len){
+	int flag=0;
+	int sum=0;
+	for(int i=0;i<len;i++)sum+=arr[i];
+	if(sum%2!=0)return flag;
+	int psetcount=1<<len;
+	int* sumarr=malloc(sizeof(int)*psetcount)
+	sum1=0;
+	for(int i=0;i<psetcount;++i){
+		sum1=0;
+		for(int j=0;j<len;++j){
+			if(i & (1<<j)){
+				sum1+=arr[j];
+			}
+			sumarr[i]=sum1;
+		}
+	}
+	int count=0;
+	for(int i=0;i<psetcount;i++){
+		if(sumarr[i]==sum)count++
+ 	if(count==2)return count;
+	}
+	return 0;
+}
+
+int main(){
+	int len;
+	scanf("%d",&len);
+	int *arr=malloc(sizeof(int)*len);
+	for(int i=0;i<len;i++){
+		scanf("%d",arr[i]);
+	}
+	if(partitionprob(arr,len))printf("Subsets found\n");else printf("subsets not found\n");
+}
