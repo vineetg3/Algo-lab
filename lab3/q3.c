5eaebf0049bec5ee810edb9f9a9408c32f957ecd
vineet <vineet.gandham@gmail.com>
2020-01-23 21:54:33 +0530

    c1

--- lab3/q3.c
+++ lab3/q3.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -7,9 +7,10 @@ int partitionprob(int * arr,int len){
 	int sum=0;
 	for(int i=0;i<len;i++)sum+=arr[i];
 	if(sum%2!=0)return flag;
+printf("AAAA\n");
 	int psetcount=1<<len;
-	int* sumarr=malloc(sizeof(int)*psetcount)
-	sum1=0;
+	int* sumarr=malloc(sizeof(int)*psetcount);
+	int sum1=0;
 	for(int i=0;i<psetcount;++i){
 		sum1=0;
 		for(int j=0;j<len;++j){
@@ -19,10 +20,11 @@ int partitionprob(int * arr,int len){
 			sumarr[i]=sum1;
 		}
 	}
+	//for(int i=0;i<psetcount;i++)printf("%d\n",sumarr[i] );
 	int count=0;
 	for(int i=0;i<psetcount;i++){
-		if(sumarr[i]==sum)count++
- 	if(count==2)return count;
+		if(sumarr[i]==sum/2)count++;
+ 	if(count>2)return count;
 	}
 	return 0;
 }
@@ -32,7 +34,7 @@ int main(){
 	scanf("%d",&len);
 	int *arr=malloc(sizeof(int)*len);
 	for(int i=0;i<len;i++){
-		scanf("%d",arr[i]);
+		scanf("%d",&arr[i]);
 	}
 	if(partitionprob(arr,len))printf("Subsets found\n");else printf("subsets not found\n");
 }
